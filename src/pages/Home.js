015f25f62edf8568821a2e30dc31cd8c63f33fc1
Fulya <fulya.ertay@gmail.com>
2023-01-04 01:07:38 +0300

    feat:added icons on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 function Home() {
   return (
     <div>Home</div>
