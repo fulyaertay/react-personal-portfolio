d25fbc4a74838b8a46242c42bb3e9de6ba3efbc6
Fulya <fulya.ertay@gmail.com>
2023-01-03 21:18:49 +0300

    feat: define route paths

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/projects" element={<Projects></Projects>}></Route>
+        <Route path="/experience" element={<Experience></Experience>}></Route>
+        </Routes>
+      </Router>
      
     </div>
   );
