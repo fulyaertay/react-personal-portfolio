9bfb91a7c2299aefe73a4f3d05c34213baefd9fc
Fulya <fulya.ertay@gmail.com>
2023-01-04 02:10:37 +0300

    feat:edit experience content

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -10,15 +10,21 @@ function Experience() {
         <h2 className='expTitle'>Experiences</h2>
         <ol>
           <li>
-            <SchoolIcon>  </SchoolIcon>
-              <h3>Associate Degree of Computer Programming (2016)</h3>
-              <span> Yaşar University</span>
+              <BusinessCenterIcon> </BusinessCenterIcon>
+              <h3>QA Tester (December 2021 - July 2022)</h3>
+              <span> Univenn</span>
           
           </li>
           <li>
-            <BusinessCenterIcon> </BusinessCenterIcon>
-              <h3>Bachelor's of Science in Computer Engineering(2020)</h3>
-              <span> Yaşar University</span>
+              <BusinessCenterIcon> </BusinessCenterIcon>
+              <h3>Software Developer Intern(July 2019 - August 2019)</h3>
+              <span> 9 Eylül University Rectorship </span>
+    
+          </li>
+          <li>
+              <BusinessCenterIcon> </BusinessCenterIcon>
+              <h3>Computer Programmer Intern(June 2015 - August 2015)</h3>
+              <span> 9 Eylül University Rectorship </span>
     
           </li>
         </ol>
