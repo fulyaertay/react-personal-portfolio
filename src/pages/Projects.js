b2f6f2c01afdf32ad8c9e216b18cfbfba3d48895
Fulya <fulya.ertay@gmail.com>
2023-01-04 13:53:09 +0300

    feat:add project links

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ProjectItem from '../components/ProjectItem'
 import { ProjectList } from '../helpers/ProjectList'
-
+import {Link} from 'react-router-dom'
 function Projects() {
   return (
     <div className='projects'>
       <h1 className='projectTitle'>My Personal Projects</h1>
       <div className='projectList'>
         {ProjectList.map((project)=>{
-          return  <ProjectItem name={project.name} image={project.image} projectLink={project.projectLink}></ProjectItem>
+          return  <a href={project.projectLink}><ProjectItem name={project.name} image={project.image} projectLink={project.projectLink}></ProjectItem></a>
         })}
       </div>
     </div>
